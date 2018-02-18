e0f05f183a878d85903b9ab9934ccb6cecb25852
hoodielive <hoodielive@gmail.com>
2018-02-18 16:55:46 -0500

    Straigten out App.js

--- app.js
+++ app.js
@@ -6,21 +6,48 @@ const app = express();
 
 // authenticate users with google
 password.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret
-        callbackURL: '/auth/google/callback'
-      },
-            (accessToken) => {
-              console.log(accessToken); i
-       }
-   )
+  new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken) => {
+      console.log(accessToken);
+      i
+    }
+  )
 );
 
 app.get('/auth/google', passport.authentication('google', {
-    scope: ['profile', 'email']
-});
+      scope: ['profile', 'email']
+    });
 
-const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+    app.listen(PORT);const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('password-google-oauth20').Strategy;
+const keys = require('./config/keys');
+const app = express();
+
+// authenticate users with google
+password.use(
+  new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken) => {
+      console.log(accessToken);
+      i
+    }
+  )
+);
+
+app.get('/auth/google', passport.authentication('google', {
+      scope: ['profile', 'email']
+    });
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT);
