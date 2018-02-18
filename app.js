9b1893f5bc2a9ae5234b2a9b8a63fb88f6cb36c2
hoodielive <hoodielive@gmail.com>
2018-02-18 16:47:00 -0500

    Working on app logic - for google OAuth

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('password-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
+// authenticate users with google
+password.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret
+        callbackURL: '/auth/google/callback'
+      },
+            (accessToken) => {
+              console.log(accessToken); i
+       }
+   )
+);
 
-
-
-password.use(new GoogleStrategy()); // authenticate users with google
-
+app.get('/auth/google', passport.authentication('google', {
+    scope: ['profile', 'email']
+});
 
 const PORT = process.env.PORT || 5000;
 
