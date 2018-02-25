200e59d9a158e9b8f62b93d2e3a4acb69b168c2a
hoodielive <hoodielive@gmail.com>
2018-02-25 11:44:09 -0500

    Refactoring

--- app.js
+++ app.js
@@ -4,21 +4,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 const app = express();
 
-// authenticate users with google
-passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-      },
-            (accessToken, refreshToken, profile, done) => {
-	    	console.log('access token', accessToken);
-	    	console.log('refresh token', refreshToken);
-	    	console.log('profile:', profile);
-	} 
-    )
-);
 
 const PORT = process.env.PORT || 5000;
 
