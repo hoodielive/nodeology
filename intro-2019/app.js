720c5b9f50f4e1bac550a9d78f0e1d519f0e8605
hoodielive <hoodielive@gmail.com>
2019-01-13 17:23:53 -0500

    module exercise

--- intro-2019/app.js
+++ intro-2019/app.js
@@ -1,3 +1,28 @@
-const lib = require('./lib')
+const { getPostsForUser, getUserById } = require('./api')
 
-lib.action(); 
+const showPostsForCurrentUser = (userId, cb) => {
+  getPostsForUser(userId, posts => {
+    const postTemplates = posts.map(post => {
+      return `
+        ${post.title}
+        ${post.body}
+        ${post.createdBy}
+        `
+    })
+    cb(postTemplates)
+  })
+}
+
+const showUserProfile = (userId, cb) => {
+  const getUserById(userId, user => {
+    const profile = `
+        ${user.name}
+    `
+    cb(user)
+  })
+}
+
+modules.exports = {
+  showPostsForCurrentUser,
+  showUserProfile
+}
